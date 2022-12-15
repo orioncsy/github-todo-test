5f82f7737c697941bc964644b647689d403a458e
orioncsy <orioncsy@gmail.com>
2022-12-15 19:39:15 +0900

    initial

---
+++ src/test/java/com/example/github_todo_test/GithubTodoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_todo_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTodoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
